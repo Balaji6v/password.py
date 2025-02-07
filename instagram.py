897103b22b4e4df0867a57ab8bcc131075210b0c
Balaji6v <119289607+Balaji6v@users.noreply.github.com>
2025-02-07 19:27:24 +0530

    Update instagram.py

--- instagram.py
+++ instagram.py
@@ -4,27 +4,25 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-# Set up the WebDriver in headless mode
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-# Open the Instagram page
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+options.add_argument("--disable-gpu")
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+
 url = "https://www.instagram.com/guviofficial/"
 driver.get(url)
 
 
 time.sleep(5)
 
-try:
 
-    followers = driver.find_element(By.XPATH, "//ul/li[1]/div/span/span").text
-    following = driver.find_element(By.XPATH, "//ul/li[3]/div/span/span").text
+followers = driver.find_element(By.XPATH, "//ul/li[1]/div/span/span").text
+following = driver.find_element(By.XPATH, "//ul/li[3]/div/span/span").text
 
-
-    print(f"Followers: {followers}")
-    print(f"Following: {following}")
-
-except Exception as e:
-    print("Error:", e)
-
+print(f"Followers: {followers}")
+print(f"Following: {following}")
 
 driver.quit()
